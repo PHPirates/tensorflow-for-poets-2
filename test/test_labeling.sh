a319c5734e663415338d067a4217606bcc5e8695
Thomas <hollandpirates@gmail.com>
2018-10-29 16:41:01 +0100

    Add wd note

--- test/test_labeling.sh
+++ test/test_labeling.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Working directory is top-level
+# Working directory should be top-level (or empty) in run config
 python -m scripts.label_image --graph=tf_files/retrained_graph_inception.pb  --labels=tf_files/retrained_labels_inception.txt --image=test/nature.jpg
--- test/test_train.sh
+++ test/test_train.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Tested, setup from  https://towardsdatascience.com/training-inception-with-tensorflow-on-custom-images-using-cpu-8ecd91595f26
+# Working directory should be top-level (or empty) in run config
+
 python -m scripts.retrain --bottleneck_dir=tf_files/bottlenecks --how_many_training_steps=4000 --model_dir=tf_files/models/ --summaries_dir=tf_files/training_summaries/"inceptionv3_nothing" --output_graph=tf_files/retrained_graph_inception_nothing.pb --output_labels=tf_files/retrained_labels_inception_nothing.txt --image_dir=tf_files/flower_photos
